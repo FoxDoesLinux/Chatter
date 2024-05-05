b3670256b1b8ebf61788e68c499cb345d6993d80
FoxDoesLinux <155398644+FoxDoesLinux@users.noreply.github.com>
2024-05-05 16:13:18 -0400

    Updated command syntax to make more sense.

--- Chatter.sh
+++ Chatter.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 rm -f data.sh
-echo "Making or connecting to a server? (making/connecting): "
+echo "Making or connecting to a server? (make/connect): "
 read INPUT
 
-if [[ $INPUT == "making" ]]; then
+if [[ $INPUT == "make" ]]; then
   touch data.sh
   echo "#!/bin/bash" > data.sh
   echo "nc -v -l 50000" >> data.sh
   chmod +x data.sh
   bash data.sh
-elif [[ $INPUT == "connecting" ]]; then
+elif [[ $INPUT == "connect" ]]; then
   echo "Enter target IP: "
   read IP
   touch data.sh
